609f195fd09868579901c3625438a42de417f470
janorihak <Janko@192.168.1.18>
2016-03-30 04:41:08 +0200

    neznam nic

---
+++ src/hackaton/reporting/string.java
@@ -0,0 +1,10 @@
+package hackaton.reporting;
+
+public class string {
+
+	public static void main(String args[]) {
+		char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+		String helloString = new String(helloArray);
+		System.out.println(helloString);
+	}
+}
